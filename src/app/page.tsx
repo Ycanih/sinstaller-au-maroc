4521cb87521e9aa1722aa92e771b02832d2c4e7a
youss bond <amghar_youssef@hotmail.com>
2026-02-28 12:01:20 +0000

    Add homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>S’installer au Maroc</h1>
+      <p>Plateforme d’accompagnement 360° pour expatriés, investisseurs et familles.</p>
+    </main>
+  );
+}
